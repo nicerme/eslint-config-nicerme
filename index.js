e9679aa0cb8506bd45fe4f03327990f51666b81a
MichałN <michal.nicer@acaisoft.com>
2019-05-14 20:55:31 +0200

    change sort rule and check if import will be working

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const { overrides, ...prettierRules } = require("./prettier.config");
+
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier', 'prettier/react'],
-  plugins: ['prettier'], // alternative: https://github.com/prettier/prettier-eslint
+  plugins: ['prettier'],
   env: {
     browser: true,
     es6: true,
@@ -17,30 +19,19 @@ module.exports = {
     },
   },
   rules: {
-    // general ESLint rules
     'no-case-declarations': 0,
     'no-plusplus': 0,
     'sort-imports': 'off',
-    'sort-keys': ['error', 'asc', { caseSensitive: true, natural: true }],
+    'sort-keys': 0,
 
-    // rules for https://github.com/benmosher/eslint-plugin-import
     'import/order': ['error', { 'newlines-between': 'always' }],
     'import/prefer-default-export': 0,
 
-    // rules for https://github.com/prettier/eslint-plugin-prettier
     'prettier/prettier': [
       'error',
-      {
-        // keep this in sync with .prettier.config.js (or .prettierrc)
-        bracketSpacing: true,
-        printWidth: 80,
-        semi: true,
-        singleQuote: true,
-        trailingComma: 'all',
-      },
+      prettierRules
     ],
 
-    // rules for https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb
     'react/jsx-curly-brace-presence': 'never', // use '' when passing a string as a property
     'react/jsx-filename-extension': 0, // we assume we do not use *.jsx files
     'react/sort-comp': 2,
